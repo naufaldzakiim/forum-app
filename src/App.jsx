9bf1774b0b8c3a38e8b15db8af155612e1bf13d2
naufaldzakiim <naufalimtiyaz35@gmail.com>
2023-07-20 00:27:44 +0700

    initial set up

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <h1 className="text-sm font-bold underline text-red-800">
+      Hello world!
+    </h1>
+  );
+}
+
+export default App;
