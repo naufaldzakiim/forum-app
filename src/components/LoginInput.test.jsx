9bd3d48c865877f5e77fbb75412de76fa727b5a8
naufaldzakiim <naufalimtiyaz35@gmail.com>
2023-07-26 12:26:17 +0700

    add LoginInput & RegisterInput component test

---
+++ src/components/LoginInput.test.jsx
@@ -0,0 +1,66 @@
+/* eslint-disable no-undef */
+/**
+ * skenario testing
+ *
+ * - LoginInput component
+ *   - should handle email typing correctly
+ *   - should handle password typing correctly
+ *   - should call login function when login button is clicked
+ */
+
+import React from 'react';
+import { cleanup, render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import LoginInput from './LoginInput';
+
+describe('LoginInput component', () => {
+  afterEach(() => {
+    cleanup();
+  });
+
+  it('should handle email typing correctly', async () => {
+    // Arrange
+    render(<LoginInput login={() => {}} />);
+    const emailInput = await screen.getByPlaceholderText('Email');
+
+    // Action
+    await userEvent.type(emailInput, 'emailtest');
+
+    // Assert
+    expect(emailInput).toHaveValue('emailtest');
+  });
+
+  it('should handle password typing correctly', async () => {
+    // Arrange
+    render(<LoginInput login={() => {}} />);
+    const passwordInput = await screen.getByPlaceholderText('Password');
+
+    // Action
+    await userEvent.type(passwordInput, 'passwordtest');
+
+    // Assert
+    expect(passwordInput).toHaveValue('passwordtest');
+  });
+
+  // ... skenario pengujian lainnya
+
+  it('should call login function when login button is clicked', async () => {
+    // Arrange
+    const mockLogin = jest.fn();
+    render(<LoginInput login={mockLogin} />);
+    const emailInput = await screen.getByPlaceholderText('Email');
+    await userEvent.type(emailInput, 'emailtest');
+    const passwordInput = await screen.getByPlaceholderText('Password');
+    await userEvent.type(passwordInput, 'passwordtest');
+    const loginButton = await screen.getByRole('button', { name: 'Login' });
+
+    // Action
+    await userEvent.click(loginButton);
+
+    // Assert
+    expect(mockLogin).toHaveBeenCalledWith({
+      email: 'emailtest',
+      password: 'passwordtest',
+    });
+  });
+});
